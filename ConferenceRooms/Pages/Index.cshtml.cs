ffd88466ae90466aea1e557c822ef4a765131757
Chris McClurken <cmcclurken@olympicmedical.org>
2023-04-19 11:09:32 -0700

    Modified for production.  now takes parameters from URL

--- ConferenceRooms/Pages/Index.cshtml.cs
+++ ConferenceRooms/Pages/Index.cshtml.cs
@@ -22,12 +22,23 @@ namespace ConferenceRooms.Pages
 
         public List<Meeting> allMeetings { get; set; }
         public string nowtime = DateTime.Now.ToString("hh:mm tt");
-        public string nowday = DateTime.Today.ToString("MMMM d");
-
+        public string nowday = DateTime.Today.ToString("dddd, MMMM dd, yyyy ");
+        public string DisplayName { get; set; }
+      
         public void OnGet()
         {
-            // Call the Graph API query.  It's going to return a calendar object
-            var meeting = getUsersAsync().GetAwaiter().GetResult();
+        // If it's debug then let's just give it a test conference room
+        // If not then read the parameters out of the URL
+#if DEBUG
+            string MeetingRoom = "mikesconferenceroom@olympicmedical.org";
+            DisplayName = "Mike E's Conference Room";
+#else
+            string MeetingRoom = Request.Query["meetingroom"].ToString();
+            DisplayName = Request.Query["displayname"].ToString();
+#endif 
+
+        // Call the Graph API query.  It's going to return a calendar object
+        var meeting = getUsersAsync(MeetingRoom).GetAwaiter().GetResult();
 
             // make a list based on your class in Models
             allMeetings = new List<Meeting>();
@@ -43,18 +54,19 @@ namespace ConferenceRooms.Pages
                     organizer = meet.Organizer.EmailAddress.Name,
                     subject = meet.Subject,
                     location = meet.Location.DisplayName,
+                    // Time comes in from EXO in UTC so convert it back!
                     start = DateTime.Parse(meet.Start.DateTime).ToLocalTime(),
                     end = DateTime.Parse(meet.End.DateTime).ToLocalTime(),
 
                 });
             }
         }
-        public async static Task<ICalendarCalendarViewCollectionPage> getUsersAsync()
+        public async static Task<ICalendarCalendarViewCollectionPage> getUsersAsync(string MeetingRoom)
         {
             //  Set up your auth keys for connecting to MS
             var clientId = "5ee5b7fa-94ec-4ab2-886f-ee1e60f96a29";
             var tenantId = "eadb5e7c-6064-4235-bdb7-5e648530bb77";
-            var clientSecret = "r.2RD~XsdIJZ9t6SA_szTG5bb0_0v27Uzz";
+            var clientSecret = "GF+GlAm0j1a4QsOad2wZo/xjoRW2AZ/7NhtIAcgKX60=";
 
             // Instantiate the application
             IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
@@ -67,19 +79,17 @@ namespace ConferenceRooms.Pages
             ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClientApplication);
             GraphServiceClient graphClient = new GraphServiceClient(authProvider);
 
-            // Choose a user to get the calendar for
-            string userName = "testconfroom@olympicmedical.org";
-
-            // Get a list of calendars for that user
+            // Get a list of calendars for the conference room passed in from onget()
             var calendars = await graphClient
-                .Users[userName].Calendars
+                .Users[MeetingRoom].Calendars
                 .Request()
                 .GetAsync();
 
             // Here is the window we'll be grabbing meetings from
-            var startingAfter = DateTime.UtcNow;
-            var endingBefore = DateTime.UtcNow.AddDays(30);
-
+            DateTime startingAfter = DateTime.UtcNow;
+            DateTime endingBefore = (DateTime.Today.AddDays(1).AddSeconds(-1));
+            // Gotta convert to UTC
+            endingBefore = endingBefore.ToUniversalTime();
             // Use them to set up query options
             var options = new List<QueryOption>();
             options.Add(new("startDateTime", startingAfter.ToString("o")));
@@ -90,7 +100,7 @@ namespace ConferenceRooms.Pages
 
             // Now go back and query Graph for all events in the Calendar using the start and end date in options.
             var events = await graphClient
-                .Users[userName].Calendars[$"{calendar.Id}"].CalendarView
+                .Users[MeetingRoom].Calendars[$"{calendar.Id}"].CalendarView
                 .Request(options)
                 .GetAsync();
 
